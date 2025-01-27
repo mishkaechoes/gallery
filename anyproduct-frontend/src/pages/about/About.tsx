492fa9988d9eda3274bfc05cf11cb0bcba1da89a
Michael Kofman <1927197+mishkaechoes@users.noreply.github.com>
2025-01-27 13:36:23 -0600

    About page

--- anyproduct-frontend/src/pages/about/About.tsx
+++ anyproduct-frontend/src/pages/about/About.tsx
@@ -5,7 +5,10 @@ export const About = (): JSX.Element => {
   return (
     <BaseLayout>
       <h3>About Us</h3>
-      We're all about React server-side rendering 📟.
+      At AnyCompany, we don’t just sell unicorns—we deliver magic. Our unicorns are handpicked (or hoof-picked!) for their unmatched rarity, vibrant beauty, and exceptional quality. We believe that every unicorn should spark wonder and bring a touch of the extraordinary to your life. That’s why we go above and beyond to ensure our unicorns are not only the finest but also the most enchanting.
+      <br/>
+      <br/>
+      From dazzling designs to meticulous craftsmanship, our unicorns are unlike anything you’ve seen before. At AnyCompany, we’re committed to making your dreams come true—because when it comes to unicorns, only the best will do. Choose AnyCompany, where magic meets perfection! 🦄✨
     </BaseLayout>
   )
 }
